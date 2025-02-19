2a7bf487ab02792b9c8f51422ed7ed4df44b2317
jologscodes <dhemarodriguez53@gmail.com>
2025-02-19 22:38:19 +0800

    Updates

--- assets/swipe-back-legacy-i4lM8FnH.js
+++ assets/swipe-back-legacy-i4lM8FnH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnVuhgg5.js","./index3-legacy-Bq6HSAWf.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BoSHZZ8W.js","./index3-legacy-Cw10fMwT.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
