7bf77d401b467cb38328d05e98665a5f40ff2c24
Jologscodes <dhemarodriguez53@gmail.com>
2025-03-07 21:43:20 +0800

    Updates

--- assets/ios.transition-legacy-CHoiP1TA.js
+++ assets/ios.transition-legacy-CHoiP1TA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-zb2KXgqd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BWFIhlDb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BpUKZUkQ.js
+++ assets/swipe-back-legacy-BpUKZUkQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-zb2KXgqd.js","./index3-legacy-BHBq_7N5.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BWFIhlDb.js","./index3-legacy-B15KOmm1.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
