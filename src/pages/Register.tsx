e9e698524365a3b57257248c0d9435bc4b1912d2
Jologscodes <dhemarodriguez53@gmail.com>
2025-03-19 00:26:45 +0800

    added Register.tsx file

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { 
+    IonAvatar,
+    IonButton,
+    IonContent, 
+    IonInput, 
+    IonInputPasswordToggle, 
+    IonItem, 
+    IonPage, 
+    useIonRouter
+  } from '@ionic/react';
+  import React, { useState } from 'react';
