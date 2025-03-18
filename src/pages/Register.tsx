ecdfd8bd5b6147d55fdeb40f025677e1c94ea624
Jologscodes <dhemarodriguez53@gmail.com>
2025-03-19 00:29:11 +0800

    added const Login, const doLogin, and const doSignup in Register.tsx file

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -9,3 +9,19 @@ import {
     useIonRouter
   } from '@ionic/react';
   import React, { useState } from 'react';
+  const Login: React.FC = () => {
+    const navigation = useIonRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const doLogin = () => {
+      if (!email || !password) {
+        alert('Please enter your email and password.');
+        return;
+      }
+      navigation.push('/it35-lab/app', 'forward', 'replace'); 
+    };
+  
+    const doSignup = () => {
+      navigation.push('/Register', 'forward', 'replace'); 
+    };
