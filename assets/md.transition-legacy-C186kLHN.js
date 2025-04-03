9fa3b21ab1bca247cd6c772214b64fc1fcbbb7ae
Jologscodes <dhemarodriguez53@gmail.com>
2025-04-03 22:35:59 +0800

    Updates

--- assets/md.transition-legacy-C186kLHN.js
+++ assets/md.transition-legacy-C186kLHN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DLc0--sJ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DlKnQo5x.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BCQpm6Db.js
+++ assets/swipe-back-legacy-BCQpm6Db.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DLc0--sJ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DlKnQo5x.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
