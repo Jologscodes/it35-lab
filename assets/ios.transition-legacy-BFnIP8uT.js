74d99c887351262007e0c5dc11053a933ddd55a8
Jologscodes <dhemarodriguez53@gmail.com>
2025-04-07 13:12:20 +0800

    Updates

--- assets/ios.transition-legacy-BFnIP8uT.js
+++ assets/ios.transition-legacy-BFnIP8uT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DlKnQo5x.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-MNetclUH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
