cf79838a1b6cbf04240e03fd2ad10ddbffe27292
Jologscodes <dhemarodriguez53@gmail.com>
2025-04-10 13:53:01 +0800

    added feed container on feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,25 +1,26 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
+
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <div
           style={{
             display: 'flex',
@@ -28,11 +29,12 @@ import {
             height: '100%',
           }}
         >
-        DANMARK PALAUTOG
+          Feed
         </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Feed;
+        <FeedContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
