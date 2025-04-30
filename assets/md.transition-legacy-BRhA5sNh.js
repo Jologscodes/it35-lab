d38a6b00dd7011ce607a8524f1cd84d614583e89
Jologscodes <dhemarodriguez53@gmail.com>
2025-04-30 21:47:45 +0800

    Updates

--- assets/md.transition-legacy-BRhA5sNh.js
+++ assets/md.transition-legacy-BRhA5sNh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-60DHjWZt.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BVxI6vdO.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
